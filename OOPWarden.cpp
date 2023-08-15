00d56b3f9f2283da944052c9117700c7bf13c617
Ish <93486637+IsmaeelHashimi@users.noreply.github.com>
2023-08-15 01:11:52 -0400

    Update OOPWarden.cpp
    Added updateCredential, deleteCredential, searchCredentials, began composing switch function to tie program.

--- OOPWarden.cpp
+++ OOPWarden.cpp
@@ -1,5 +1,5 @@
-// OOPWarden.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// OOPWarden.cpp 
+// Ismaeel Hashimi
 
 #include <iostream>
 #include <string>
@@ -63,6 +63,37 @@ public:
         credentials.emplace(website, Credential(website, username, password));
     }
 
+    void updateCredential(const string& website, const string& username, const string& password) {
+        auto it = credentials.find(website);
+        if (it != credentials.end()) {
+            it->second.setUsername(username);
+            it->second.setPassword(password);
+        }
+        else {
+            cout << "Credential not found for website: " << website << endl;
+        }
+    }
+
+    void deleteCredential(const string& website) {
+        auto it = credentials.find(website);
+        if (it != credentials.end()) {
+            credentials.erase(it);
+        }
+        else {
+            cout << "Credential not found for website: " << website << endl;
+        }
+    }
+
+    void searchCredentials(const string& website) const {
+        auto it = credentials.find(website);
+        if (it != credentials.end()) {
+            it->second.display();
+        }
+        else {
+            cout << "Credential not found for website: " << website << endl;
+        }
+    }
+
     void displayCredentials() const {
         for (const auto& pair : credentials) {
             pair.second.display();
@@ -79,8 +110,10 @@ private:
 int main()
 {
     User user("myUser", "masterPassword");
-
+    string website;
+    string username;
     string password;
+    int userInput;
     cout << "Enter master password: " << endl;
     cin >> password;
 
@@ -91,6 +124,35 @@ int main()
 
     user.addCredential("example.com", "user", "password");
     user.displayCredentials();
+    bool keepGoing = true;
+    while (keepGoing) {
+        cout << "What option do you want to select?: " << endl;
+        cout << "[1] Add a credential" << endl;
+        cout << "[2] Update a credential" << endl;
+        cout << "[3] Delete a credential" << endl;
+        cout << "[4] Check if a credential exists" << endl;
+        cout << "[9] Quit program" << endl;
+        cin >> userInput;
+        switch (userInput) {
+        case 1:
+            cout << "What is the name of the website you want to add credentials for?: ";
+            cin >> website;
+            cout << endl;
+            cout << "What is your username for this website?: ";
+            cin >> username;
+            cout << endl;
+            cout << "What is your password for this website?: ";
+            cin >> password;
+            cout << endl;
+            user.addCredential(website, username, password);
+            break;
 
+        case 9:
+            cout << "Thank you for using OOPWarden, continue keeping your passwords safe." << endl;
+            keepGoing = false;
+            return -1;
+        }
+    }
+    
     return 0;
 }
